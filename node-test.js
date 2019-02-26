ca7d67ac35807ec33095c566265014d127c89ed5
Andy Campbell <andrew_campbell1@hotmail.com>
2019-02-26 14:58:56 +1100

    create node file

---
+++ node-test.js
@@ -0,0 +1,17 @@
+var banana = "hello node world";
+
+console.log(banana);
+
+var name1 = "Andrew";
+
+console.log(name1 + " likes to say " + banana);
+
+var apple = 6
+
+var grape = 10
+
+console.log(apple+grape);
+
+var name2 = "Jordyn";
+
+console.log(name1 + " and " + name2 + " have the same class");
